3f3b15f55255fb6c9c11d66371a6b715e2995ae6
hogdenmoore <hogdenmoore@gmail.com>
2022-07-07 10:11:41 -0700

    modified the main compoennt so it did not break

--- src/components/SeamCarver.js
+++ src/components/SeamCarver.js
@@ -61,14 +61,18 @@ const SeamCarver = () => {
   }, [initialImg]);
 
   const onPictureClick = async () => {
+    console.log(isMoving);
     if (isMoving === 1) {
+      console.log("i");
       setInitialImg(initialImage);
       setCanvasStaging(true);
     }
   };
-
   const resize = async () => {
-    if (isMoving === 2 || isMoving === 3) {
+    console.log(imgRef.current);
+    if (isMoving === 2 || isMoving === 3 || imgRef.current === null) {
+      setIsMoving(1);
+      console.log("p");
       return;
     }
     setIsMoving(2);
